b9cc5c05dfe37da77d01f18af98ec746d3d08205
caleyrrrrrrrrrrrr <117769242+caleyrrrrrrrrrrrr@users.noreply.github.com>
2025-08-30 18:55:46 +0100

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -64,18 +64,6 @@ const Home = () => {
         author4Name={<span className="home-text41">Wendy</span>}
       />
     </div>
-    <div style={{ width: "100%", height: "400px" }}>
-      <iframe
-        title="8b The Bookcase Location"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2453.69517397041!2d-3.4661!3d50.5817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486dcba12d1e0001%3A0x123456789abcdef!2sDawlish%2C%20UK!5e0!3m2!1sen!2suk!4v1691234567890!5m2!1sen!2suk"
-        width="100%"
-        height="100%"
-        style={{ border: 0 }}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-    </div>
   )
 }
 
