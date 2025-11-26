7b83c44e34dab987b113ce9cacb761b97fb0f0e3
caleyrrrrrrrrrrrr <117769242+caleyrrrrrrrrrrrr@users.noreply.github.com>
2025-11-26 18:25:18 +0000

    fix index cleaning to dawlish

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {
 import './style.css'
 import Home from './views/home'
 import Contact from './views/contact'
-import Cleaning from './views/cleaning'
+import Cleaning from './views/dawlish'
 import NotFound from './views/not-found'
 
 const App = () => {
